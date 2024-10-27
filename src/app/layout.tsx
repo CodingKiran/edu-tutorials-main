c007810411c1ca0bcfc1f40b64600dc97c4bf495
Kiran Kumar <102020044+CodingKiran@users.noreply.github.com>
2024-10-27 13:34:59 +0530

    title removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
 import "./globals.css";
-import type { Metadata } from "next";
+
 import { inter } from "@/app/ui/fonts";
 import Navbar from "@/components/Navbar/page";
 import Footer from "@/components/Footer/page";
-
-export const metadata: Metadata = {
-  title: "Edu Tutorials",
-  description: "A platform for commerce students to learn the concepts easily",
-};
 
 export default function RootLayout({
   children,
